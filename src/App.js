1ca1b1113569667aa39c1e0f4653c8cdadbc3a20
attaka <tomorrowneverknowssaimin@gmail.com>
2023-04-04 01:09:15 +0900

    form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Title from "./components/Title";
+import Form from "./components/Form";
 import "./App.css";
 
 function App() {
   return (
     <div className="test">
       <Title />
+      <Form />
     </div>
   );
 }
